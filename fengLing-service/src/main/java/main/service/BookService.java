82c643ea94780f53f8a54bdf35e08462995de727
Chen-Fengling <245828824@qq.com>
2017-04-08 19:40:18 +0800

    将三个Web Modules 合并为一个

---
+++ fengLing-service/src/main/java/main/service/BookService.java
@@ -0,0 +1,19 @@
+package main.service;
+
+
+import main.domain.Book;
+import main.domain.Category;
+
+import java.util.List;
+
+public interface BookService {
+    
+    List<Category> getAllCategories();
+    Category getCategory(int id);
+    List<Book> getAllBooks();
+    Book save(Book book);
+    Book update(Book book);
+    Book get(long id);
+    long getNextId();
+
+}
